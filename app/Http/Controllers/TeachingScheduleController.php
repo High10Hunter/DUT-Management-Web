52602b28bccfc4dcefb170a6339d5b363ca05ef0
Tran Dai Quy <trandaiquy2903@gmail.com>
2022-10-18 16:38:09 +0700

    refactor(lecturer - view teaching schedule): create getSchedules function

--- app/Http/Controllers/TeachingScheduleController.php
+++ app/Http/Controllers/TeachingScheduleController.php
@@ -43,72 +43,8 @@ class TeachingScheduleController extends Controller
             '#daa520',
         ];
 
-        $schedule = [];
-        foreach ($modules as $index => $module) {
-            $beginDate = $module->begin_date;
-            $beginDate = Carbon::createFromFormat('Y-m-d', $beginDate);
+        $schedules = Module::getSchedules($modules, $colors);
 
-            $scheduleDays = $module->schedule;
-
-            $lessons = $module->lessons;
-            $moduleName = $module->subject->name . ' - ' . $module->name;
-
-            $startSlot = Carbon::createFromFormat('H:i:s', TimeSlotEnum::getStartTimeBySlotId($module->start_slot));
-            $duration = ($module->end_slot - $module->start_slot) * TimeSlotEnum::DURATION;
-            $endSlot = $startSlot->copy()->addMinutes($duration);
-
-
-            $schedule[] = [
-                "title" => $moduleName,
-                "begin_date" => $beginDate,
-                "start_slot" => $startSlot->format('H:i:s'),
-                "end_slot" => $endSlot->format('H:i:s'),
-                "color" => $colors[$index],
-            ];
-            --$lessons;
-            while ($lessons > 0) {
-                for ($i = 1; $i < count($scheduleDays); $i++) {
-                    $current = end($schedule)['begin_date'];
-                    $currentDayOfWeek = $current->dayOfWeek + 1;
-                    $offset = (int)$scheduleDays[$i] - $currentDayOfWeek;
-
-                    $next = $current->copy()->addDays($offset);
-                    $schedule[] = [
-                        "title" => $moduleName,
-                        "begin_date" => $next,
-                        "start_slot" => $startSlot->format('H:i:s'),
-                        "end_slot" => $endSlot->format('H:i:s'),
-                        "color" => $colors[$index],
-                    ];
-                    --$lessons;
-                }
-
-                if ($lessons > 0) {
-                    $startDayOfWeekIndex = (count($schedule) - 1) - (count($scheduleDays) - 1);
-                    $startDayOfWeek = $schedule[$startDayOfWeekIndex]['begin_date'];
-                    $next = $startDayOfWeek->copy()->addDays(7);
-                    $schedule[] = [
-                        "title" => $moduleName,
-                        "begin_date" => $next,
-                        "start_slot" => $startSlot->format('H:i:s'),
-                        "end_slot" => $endSlot->format('H:i:s'),
-                        "color" => $colors[$index],
-                    ];
-                    --$lessons;
-                }
-            }
-        }
-
-
-        $schedule = array_map(function ($each) {
-            return [
-                "title" => $each['title'],
-                "start" => $each['begin_date']->format('Y-m-d') . ' ' . $each['start_slot'],
-                "end" => $each['begin_date']->format('Y-m-d') . ' ' . $each['end_slot'],
-                "color" => $each['color'],
-            ];
-        }, $schedule);
-
-        return response()->json($schedule);
+        return response()->json($schedules);
     }
 }
